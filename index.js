ea20bd84d994d739a04eaef64f39f1b52454a572
Aaron McCall <aaron@andyet.net>
2014-09-26 12:53:55 -0700

    initial code

---
+++ index.js
@@ -0,0 +1,19 @@
+var slice = Array.prototype.slice;
+slice = slice.call.bind(slice.call, slice);
+
+module.exports = function _kisslog(config) {
+    return function _log() {
+        var args = slice(arguments);
+        var logArgs = args;
+        if (config && config.debug) {
+            if (typeof window === 'undefined') {
+                var arg;
+                var i = -1;
+                while ((arg = args[++i])) {
+                    if (typeof arg === 'string') logArgs[i] = arg.replace('%o', '%j');
+                }
+            }
+            console.log.apply(console, logArgs);
+        }
+    };
+};
