733333976e58b0b8d83c591293853159897e0e37
Aaron McCall <aaron@andyet.net>
2014-10-06 14:44:46 -0700

    add log level support

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 var slice = Array.prototype.slice;
 slice = slice.call.bind(slice.call, slice);
 
+var levels = {
+    debug: 1,
+    info: 2,
+    warn: 3,
+    error: 4
+};
+
 module.exports = function _kisslog(config) {
-    return function _log() {
-        var args = slice(arguments);
-        var logArgs = args;
-        if (config && config.debug) {
+    config = config || {};
+    var logLevel = levels.error;
+    if (typeof config.debug === 'number') {
+        logLevel = config.debug;
+    } else if (config.debug === true) {
+        logLevel = 1;
+    }
+    return function _log(level) {
+        var args = slice(arguments, (typeof level === 'number' && arguments.length > 1 ? 1 : 0));
+        if (typeof level !== 'number') {
+            level = 2;
+        }
+        if (level >= logLevel) {
+            var logArgs = args;
             if (typeof window === 'undefined') {
                 var arg;
                 var i = -1;
@@ -17,3 +34,7 @@ module.exports = function _kisslog(config) {
         }
     };
 };
+
+for (var key in levels) {
+    module.exports[key] = levels[key];
+}
